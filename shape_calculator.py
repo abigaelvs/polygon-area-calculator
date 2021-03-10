ec81925a6e8756558e2d527df59b835e18cb217e
abigael <abigaelvas@gmail.com>
2021-03-10 20:40:09 +0700

    First Commit

---
+++ shape_calculator.py
@@ -0,0 +1,51 @@
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return (2 * self.width) + (2 * self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        picture = ''
+        if self.width <= 50 and self.height <= 50:
+            for i in range(self.height):
+                picture += '*' * self.width + '\n'
+            return picture
+        else:
+            return 'Too big for picture.'
+        
+
+    def get_amount_inside(self, another_shape):
+        luas = self.get_area()
+        another_shape_luas = another_shape.get_area()
+        return int(luas / another_shape_luas)
+
+    def __str__(self):
+        return f'Rectangle(width={self.width}, height={self.height})'
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        self.width = length
+        self.height = length
+
+    def set_side(self, length):
+        self.width = length
+        self.height = length
+
+    def __str__(self):
+        return f"Square(side={self.width})"
